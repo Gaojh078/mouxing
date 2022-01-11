f57800683251c963509869afc10e18d41d54743e
Gaojh078 <97378318+Gaojh078@users.noreply.github.com>
2022-01-11 17:21:30 +0800

    Add files via upload

--- binning.v
+++ binning.v
@@ -6,15 +6,19 @@ input binning_valid,
 output binning_ready
 );
 parameter n = 8;
-reg [11:0] str_data_reg;
+reg [11:0] str_data_reg = 'd0;
+wire prog_full_all;
+reg prog_full_all_reg,prog_full_all_reg2;
 wire [11:0] fifo_in[n-1:0];
 wire [11:0] fifo_out[n-1:0];
 reg  [n-1:0]fifo_wen;
-reg  fifo_ren;
-reg [2:0]stream_count;
+reg  fifo_ren = 0;
+reg [2:0]stream_count = 0;
 reg [6:0]fifo_count[n-1:0];
 reg pop_en[n-1:0];
-reg rd_en;
+wire rd_en;
+wire [n-1:0]prog_full;
+assign binning_ready = 1;
 always@(posedge clk or negedge rst)
 begin
     if(!rst)
@@ -34,7 +38,7 @@ end
 always@(posedge clk or negedge rst) begin
     if(!rst)
         fifo_wen <= 'd0;
-    else begin
+    else if(binning_valid && binning_ready)begin
         case(stream_count)
         3'd0:fifo_wen <= 'b00000001;
         3'd1:fifo_wen <= 'b00000010;
@@ -47,11 +51,13 @@ always@(posedge clk or negedge rst) begin
         default:fifo_wen <= 'd0;
         endcase
     end
+    else
+    fifo_wen <= 'd0;
 end    
 generate
 genvar u;
     for(u=0;u<8;u=u+1) begin
-        assign fifo_in[u] = str_data_reg; 
+   //     assign fifo_in[u] = str_data_reg; 
         always@(posedge clk or negedge rst) begin
             if(!rst)
                 fifo_count[u] <= 'd0;
@@ -61,30 +67,47 @@ genvar u;
                         fifo_count[u] <= 'd0;
                         pop_en[u] <= 1;
                     end
-                    else 
+                    else begin
                         fifo_count[u] <= fifo_count[u] +1;
+                        pop_en[u] <= 0;
+                    end
                 end
             end
         end    
                    
         fifo_generator fifo_stream2MM (
             .clk(clk),                  // input wire clk
-            .srst(rst),                // input wire srst
-            .din(fifo_in[u]),                  // input wire [11 : 0] din
+            .srst(!rst),                // input wire srst
+            .din(str_data_reg),                  // input wire [11 : 0] din
             .wr_en(fifo_wen[u]),              // input wire wr_en
             .rd_en(rd_en),              // input wire rd_en
             .dout(fifo_out[u]),                // output wire [11 : 0] dout
             .full(),                // output wire full
-            .empty(),              // output wire empty
+            .empty(),          
+            .prog_full(prog_full[u]),    // output wire empty
             .wr_rst_busy(),  // output wire wr_rst_busy
             .rd_rst_busy()  // output wire rd_rst_busy
             );
         end
 endgenerate
+assign prog_full_all = &prog_full;  
+assign rd_en = ~prog_full_all_reg & prog_full_all;
 always@(posedge clk or negedge rst) begin
     if(!rst)
-        rd_en <= 0;
-    else if({pop_en[0],pop_en[1],pop_en[2],pop_en[3],pop_en[4],pop_en[5],pop_en[6],pop_en[7]}=='b11111111)
-        rd_en <= 1;
-end    
+        prog_full_all_reg <= 0;
+    else begin
+        prog_full_all_reg <= prog_full_all;
+        
+    //    prog_full_all_reg2 <= prog_full_all_reg;
+    end
+end
+//always@(posedge clk or negedge rst) begin
+//    if(!rst)
+//        rd_en <= 0;
+//    else if(prog_full_all_reg==0 && prog_full_all == 1)
+//        rd_en <= 1;
+//    else
+//        rd_en <= 0;
+//end
+   
 endmodule
